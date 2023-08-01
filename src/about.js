83c3adaa714d9f774ac9dc57ba27a4de53c2169c
Tarik <tarikozgur0607@gmail.com>
2023-08-01 23:08:15 +0300

    new codes

--- src/about.js
+++ src/about.js
@@ -1,7 +1,6 @@
-export default function about(header,box1,box2,box3,myImg,contactImg){
+export default function about(header,box1,box2,box3){
     header.innerText = "About";
     box1.innerText = "email : XXX@gmail.com";
     box2.innerText = "phone:XXXXXXXXX";
     box3.innerText = "Lorem : XXXXXXX";
-    myImg.src = "./about.png";
- }
+   }
--- src/contact.js
+++ src/contact.js
@@ -3,4 +3,4 @@ header.innerText = "Contact";
 box1.innerText = "email : XXX@gmail.com";
 box2.innerText = "phone:XXXXXXXXX";
 box3.innerText = "Lorem : XXXXXXX";
-}
+ }
